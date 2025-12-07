fb02aaafb087f90cfd4bb8cb85ade63b7c588f46
Shreyaa <shreyaaanand6@gmail.com>
2025-12-07 15:26:20 +0530

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Filter = ({filterText, handleFilterChange}) => (
   <div>
@@ -24,13 +25,17 @@ const PersonForm = ({handleForm, newName, newNumber, handleInput, handleNumber})
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [personsToShow, setPersonsToShow] = useState(persons);
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+        setPersonsToShow(response.data);
+      })
+  }, [])
+
+  const [persons, setPersons] = useState([]);
+  const [personsToShow, setPersonsToShow] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterText, setFilterText] = useState('');
@@ -66,7 +71,6 @@ const App = () => {
 
   const filter = (event) => {
     const value = event.target.value;
-    console.log("Filtering with:", value);
     setFilterText(value);
     const filtered = persons.filter(person =>
       person.name.toLowerCase().includes(value.toLowerCase()) ||
