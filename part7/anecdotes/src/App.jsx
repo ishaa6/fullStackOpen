cfccd557579e5ddc3922bb6075dfc054e781ffef
Shreyaa <shreyaaanand6@gmail.com>
2026-01-02 11:17:22 +0530

    custom hook

--- part7/anecdotes/src/App.jsx
+++ part7/anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import { Link, Navigate, Route, BrowserRouter as Router, Routes, useMatch, useNavigate, useParams } from 'react-router-dom'
+import { Link, Route, BrowserRouter as Router, Routes, useNavigate, useParams } from 'react-router-dom'
+
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -35,7 +37,7 @@ const About = () => (
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      An anecdote is &quot;a story with a point.&quot;</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
@@ -50,23 +52,28 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
   const navigate = useNavigate()
-
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.props.value,
+      author: author.props.value,
+      infor: info.props.value,
       votes: 0
     })
-    props.setNotification(`a new anecdote ${content} created!`)
+    props.setNotification(`a new anecdote ${content.props.value} created!`)
     navigate('/', {replace:true})
+  }
+
+  const resetValue = () => {
+    content.clear()
+    author.clear()
+    info.clear()
   }
 
   return (
@@ -75,17 +82,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content.props} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author.props} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info.props}/>
         </div>
         <button>create</button>
+        <button type="reset" onClick={resetValue}>reset</button>
       </form>
     </div>
   )
---
+++ part7/anecdotes/src/hooks/index.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export const useField = (name) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const clear = () => {
+        setValue('')
+    }
+ 
+    return {
+        props: {
+            name,
+            value,
+            onChange
+        },
+        clear
+    }
+}
